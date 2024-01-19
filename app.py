8fc72c044dc79162c50f6dfc6f039ce66b56efae
elpetre <155978728+elpetre@users.noreply.github.com>
2024-01-19 10:04:31 -0800

    Updated nav

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('navbar.html')
+    return render_template('homepage.html')
+@app.route('/products')
+def products():
+    return render_template('products.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/contact-us')
+def contact_us():
+    return render_template('contact-us.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
